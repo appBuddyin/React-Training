32f945a40c5e061e88e33b3b1c403fd83dbdfeef
nigam-chauhan <nigamchauhanmbd125@gmail.com>
2019-09-17 18:07:23 +0530

    Comit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class car extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
